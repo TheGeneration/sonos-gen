d5545801b5d784afd73e1809712ebd077de38934
Martin Werner <carlmartinwerner@gmail.com>
2017-04-20 09:42:19 +0000

    Change poll interval to 15 seconds to prevent killing of Sonos

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ server.listen(PORT, function() {
 });
 
 // var sonosDevicePollInterval = 30 * 1000;
-var sonosDataPollInterval = 1000 * 1;
+var sonosDataPollInterval = 1000 * 15;
 
 var sonosDevice = false;
 
